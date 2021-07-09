7595d6a5ea3dab7c9b4daa19c50292ea202a005f
Adrian B. Cronin <cronin.adrianb@gmail.com>
2021-07-09 10:12:06 -0700

    editted gitignore file, moved array of questions to  variable

--- index.js
+++ index.js
@@ -2,23 +2,26 @@
 const inquirer = require('inquirer'); // inquirer module
 const fs = require('fs'); // file system module
 const genMark = require('./utils/generateMarkdown');
+// look up require 'path'
+
 
 // TODO: fill out prompts from README
+// put the prompt questions in the array and put this inquirer call in init
 inquirer
-  .prompt([
-    {
-      type: 'input',
-      message: 'This is the input',
-      name: 'input',
-    }
-  ])
+  .prompt(questions)
   .then((response) =>
     console.log(response.input),
   );
 
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: 'input',
+    message: 'This is the input',
+    name: 'input',
+  },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
